656ccf5cee6cefb836a552198da69aaf35a0e07f
school_app <eng.rakesh.thakral@gmail.com>
2026-08-21 20:28:12 +0530

    Configure ApplicationDbContext

--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using zioAqua.Data;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 // Add services to the container.
 builder.Services.AddControllers();
 
